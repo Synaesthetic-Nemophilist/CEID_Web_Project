82e63f877fc23108348e2260e47eb3f02ff89dc4
nomaterials <nomaterials@gmail.com>
2017-08-15 23:00:10 +0300

    mongoDB config with events

---
+++ db/config.js
@@ -0,0 +1,35 @@
+var mongoose = require('mongoose');
+
+//SHELL: mongo ds163667.mlab.com:63667/web-project-2017 -u admin -p webprojectadmin
+
+//Cloud Database Connection
+var uri = 'admin:webprojectadmin@ds163667.mlab.com:63667/web-project-2017';
+
+// Create the database connection
+mongoose.connect(uri);
+
+// CONNECTION EVENTS
+// When successfully connected
+mongoose.connection.on('connected', function () {
+    console.log('Mongoose default connection open to ' + uri);
+    //Drop the DB
+    //mongoose.connection.db.dropDatabase();
+});
+
+// If the connection throws an error
+mongoose.connection.on('error',function (err) {
+    console.log('Mongoose default connection error: ' + err);
+});
+
+// When the connection is disconnected
+mongoose.connection.on('disconnected', function () {
+    console.log('Mongoose default connection disconnected');
+});
+
+// If the Node process ends, close the Mongoose connection
+process.on('SIGINT', function() {
+    mongoose.connection.close(function () {
+        console.log('Mongoose default connection disconnected through app termination');
+        process.exit(0);
+    });
+});
--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 
-var app = express();
+// Port based on env or use default
 var port = process.env.port || 8080;
+
+//App Instantiation
+var app = express();
+
+//Db Configuration & init
+var db = require('./db/config');
+
 
 
 // Middleware
