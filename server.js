c9de9f3951d5d88113f9a3b7fbb070d577ebfe18
nomaterials <nomaterials@gmail.com>
2017-08-16 01:25:37 +0300

    serve static index

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 //Import modules
-var express = require('express');
-var morgan = require('morgan');
-var bodyParser = require('body-parser');
-var router = express.Router();
-var appRoutes = require('./app/routes/api')(router);  // pass router to routes file
+var express     = require('express');
+var morgan      = require('morgan');
+var bodyParser  = require('body-parser');
+var path        = require('path');
+
+// Fetch api-stuffed router
+var router      = express.Router();
+var appRoutes   = require('./app/routes/api')(router);  // pass router to routes file
 
 
 //App Instantiation
@@ -21,9 +24,14 @@ var db = require('./db/config');
 app.use(morgan('dev'));  // for route logging
 app.use(bodyParser.json());  // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true }));  // for parsing application/x-www-form-urlencoded
+app.use(express.static(__dirname + '/public'));  // serve public dir
 // Router Linking
 app.use('/api', appRoutes);  // use backend api routes (distinguish these with angular's routes_
 
+
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname +'/public/app/views/index.html'));
+})
 
 
 // App start & listen
