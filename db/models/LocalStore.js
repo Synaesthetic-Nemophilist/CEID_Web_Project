fd45f0bac75ea5e27850b118d08dd2abcdd5cb81
fortysixntwo <dennis.petta@gmail.com>
2017-08-21 14:06:45 +0300

    Added LocalStore model

---
+++ db/models/LocalStore.js
@@ -0,0 +1,38 @@
+/**
+ * Created by fortysixntwo on 21/08/2017.
+ */
+
+
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+
+var LocalStoreSchema = Schema({
+
+    transit_hub_id: {type: Schema.ObjectId, ref: 'TransitHub', required: true},
+    phone_number: {type: Number, required: true},
+
+    Address: [{
+        City: {type: String, required: true},
+        Street: {type: String, required: true},
+        Number: {type: String, required: true},
+        Post_code: {type: Number, required: true}
+    }],
+
+
+    Location: [{
+        Longitude: {type: String},
+        Latitude: {type: String}
+    }],
+
+    // All stored packages in the current Store
+    Stored_packages: [{
+        package_id: { type: Schema.ObjectId, ref:'Package', unique: true}
+
+    }]
+
+});
+
+
+module.exports = mongoose.model("LocalStore",LocalStoreSchema);
