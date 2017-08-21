4cd11c12d4658769842a9da25c9f4c5f4eb73a0d
nomaterials <nomaterials@gmail.com>
2017-08-21 15:34:57 +0300

    fix mainCtrl name

--- public/app/app.js
+++ public/app/app.js
@@ -2,5 +2,5 @@ angular.module('adminApp', ['adminControllers', 'adminServices']);
 angular.module('localEmpApp', ['localEmpControllers', 'localEmpServices', ]);
 
 
-angular.module('systemApp', ['Routes','adminApp', 'localEmpApp', 'mainControllers']);
+angular.module('systemApp', ['Routes', 'adminApp', 'localEmpApp', 'mainController']);
 
