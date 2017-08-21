b492a1e4022aae6c8ed7dabe78563f31def693ed
fortysixntwo <dennis.petta@gmail.com>
2017-08-21 14:57:03 +0300

    Local employee final commit

--- public/app/app.js
+++ public/app/app.js
@@ -2,5 +2,5 @@ angular.module('adminApp', ['adminControllers', 'adminServices']);
 angular.module('localEmpApp', ['Local_EmpServices','Local_EmpControllers']);
 
 
-angular.module('systemApp', ['Routes','adminApp','mainControllers']);
+angular.module('systemApp', ['Routes','adminApp','localEmpApp','mainControllers']);
 
