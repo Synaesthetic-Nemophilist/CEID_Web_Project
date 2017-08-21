0e963ad2056407087a0b0144fd1119240779291c
fortysixntwo <dennis.petta@gmail.com>
2017-08-21 13:30:12 +0300

    Updated Todo list

--- app/routes/api.js
+++ app/routes/api.js
@@ -1,5 +1,13 @@
 var Admin = require('../../db/models/Admin');
 
+//TODO: Google API
+//TODO: Pinakas elegxou tou admin
+//TODO: Login/Logout Admin
+//TODO: CRUD tou local store
+//TODO: CRUD tou local employee
+//TODO: CRUD tou transit hub employee
+
+
 
 module.exports = function (router) {
 
