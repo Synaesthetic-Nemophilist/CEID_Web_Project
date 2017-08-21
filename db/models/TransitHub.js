de4e793d2f481155235d806e42381fa41df49c1c
fortysixntwo <dennis.petta@gmail.com>
2017-08-21 14:06:54 +0300

    Added TransitHub model

---
+++ db/models/TransitHub.js
@@ -0,0 +1,21 @@
+/**
+ * Created by fortysixntwo on 21/08/2017.
+ */
+
+
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+
+
+var TransitHubSchema = Schema({
+    //longtitude: {type: String, required: true},
+    //latitude: {type: String, required: true},
+    location: {type: Array, required: true},
+    name: {type: String, required: true},
+    //local_store_id: {type: Schema.ObjectId, required: true, unique: true}
+
+});
+
+module.exports = mongoose.model("TransitHub", TransitHubSchema);
