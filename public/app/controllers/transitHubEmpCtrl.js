067f34d0b65dfa28fa4836d9837cc0b0e4ba1317
nomaterials <nomaterials@gmail.com>
2017-09-06 13:09:53 +0300

    fix camera close upon button press and successful scan

--- public/app/controllers/transitHubEmpCtrl.js
+++ public/app/controllers/transitHubEmpCtrl.js
@@ -1,19 +1,28 @@
 angular.module('transitHubEmpControllers', ['webcam', 'bcQrReader'])
 
-    .controller('qrCtrl', function () {
+    .controller('qrCtrl', function ($rootScope) {
 
         //for making ctrl vars public to scope
         let vm = this;
 
         vm.start = function() {
             vm.cameraRequested = true;
+            vm.cameraIsOn = true;
+        };
+
+        vm.stop = function () {
+            vm.cameraRequested = false;
+            vm.cameraIsOn = false;
+            $rootScope.$broadcast('STOP_WEBCAM');
         };
 
         vm.processURLfromQR = function (url) {
             vm.url = url;
             vm.cameraRequested = false;
-        }
+            vm.cameraIsOn = false;
+            $rootScope.$broadcast('STOP_WEBCAM');
 
+        };
 
     });
 
