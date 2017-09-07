db2253ed4dbee1339bd4b36706d2b356490baba4
nomaterials <nomaterials@gmail.com>
2017-09-07 20:13:08 +0300

    automatic col resize

--- public/app/controllers/homeCtrl.js
+++ public/app/controllers/homeCtrl.js
@@ -20,7 +20,7 @@ angular.module('homeControllers', ['uiGmapgoogle-maps','localStoreServices','pac
 
             center: {
                 latitude:  38.223970,
-                longitude: 23.645657
+                longitude: 29.645657
             },
             zoom: 6,
             markers: [],
@@ -286,6 +286,14 @@ angular.module('homeControllers', ['uiGmapgoogle-maps','localStoreServices','pac
                 });
         };
 
+
+        vm.getClass = function () {
+            if(vm.searchedPackage && vm.searchedPackage.Hubs_Passed.length > 0) {
+                return "col-md-6";
+            } else {
+                return "col-md-12";
+            }
+        };
 
         loadMarkers();
     });
